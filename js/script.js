677f3b5471ec9727419edacc8014bdf616a414b5
mattiagiaco <martex781227@gmail.com>
2021-11-11 15:46:32 +0100

    start

---
+++ js/script.js
@@ -0,0 +1,53 @@
+const app = new Vue({
+
+  el: '#app',
+  data:{
+    images: ['img/01.jpg',
+    'img/02.jpg',
+    'img/03.jpg',
+    'img/04.jpg',
+    'img/05.jpg'],
+
+    title:['Svezia',
+    'Svizzera',
+    'Gran Bretagna',
+    'Germania',
+    'Paradise'],
+
+    text:[
+    'Lorem ipsum, dolor sit amet consectetur adipisicingelit. Et temporibus voluptatum suscipit temporealiquid deleniti aut veniam inventore eligendi ex adullam, cumque provident totam omnis, magnam doloresdolorum corporis.',
+    'Lorem ipsum',
+    'Lorem ipsum, dolor sit amet consectetur adipisicingelit.',
+    'Lorem ipsum, dolor sit amet consectetur adipisicingelit. Et temporibus voluptatum suscipit temporealiquid deleniti aut veniam inventore eligendi ex adullam,',
+    'Et temporibus voluptatum suscipit tempore aliquiddeleniti aut veniam inventore eligendi ex ad ullam,'
+    ]
+  },
+  methods:{
+    
+  }
+})
+
+
+
+
+const image = ['img/01.jpg',
+  'img/02.jpg',
+  'img/03.jpg',
+  'img/04.jpg',
+  'img/05.jpg'
+];
+
+const title = ['Svezia',
+  'Svizzera',
+  'Gran Bretagna',
+  'Germania',
+  'Paradise'
+];
+
+const text = [
+  'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Et temporibus voluptatum suscipit tempore aliquid deleniti aut veniam inventore eligendi ex ad ullam, cumque provident totam omnis, magnam dolores dolorum corporis.',
+  'Lorem ipsum',
+  'Lorem ipsum, dolor sit amet consectetur adipisicing elit.',
+  'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Et temporibus voluptatum suscipit tempore aliquid deleniti aut veniam inventore eligendi ex ad ullam,',
+  'Et temporibus voluptatum suscipit tempore aliquid deleniti aut veniam inventore eligendi ex ad ullam,'
+];
